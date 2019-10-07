3c7f3afbec1ecab0fa06e6da839efb2116fd00e3
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-10-07 12:07:54 +0100

    Move VXC related bits into a separate file
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- megaport/api/port.go
+++ megaport/api/port.go
@@ -14,6 +14,10 @@ const (
 	ProductTypeMCR2 = "MCR2"
 )
 
+// port: virtual = false, type = MEGAPORT
+// mcr1: virtual = true , type = MEGAPORT
+// mcr2: virtual = false, type = MCR2
+
 type PortsService struct {
 	c *Client
 }
@@ -23,8 +27,8 @@ func NewPortsService(c *Client) *PortsService {
 }
 
 func (p *PortsService) Create(name string, locationId, speed, term uint64, validate bool) (string, error) {
-	payload, err := json.Marshal([]OrderPort{
-		OrderPort{
+	payload, err := json.Marshal([]portOrder{
+		portOrder{
 			// CreateDate   uint64          // TODO: need to fill in? :o
 			//LagPortCount uint64          // TODO: Required: the number of ports in this LAG order (https://dev.megaport.com/#standard-api-orders-validate-lag-order)
 			LocationId: locationId,
@@ -77,10 +81,7 @@ func (p *PortsService) Get(uid string) (*Product, error) {
 func (p *PortsService) Update() {}
 func (p *PortsService) Delete() {}
 
-// port: virtual = false, type = MEGAPORT
-// mcr1: virtual = true , type = MEGAPORT
-// mcr2: virtual = false, type = MCR2
-type OrderPort struct {
+type portOrder struct {
 	CreateDate   uint64 `json:"createDate,omitempty"`   // TODO: need to fill in? :o
 	LagPortCount uint64 `json:"lagPortCount,omitempty"` // TODO: Required: the number of ports in this LAG order (https://dev.megaport.com/#standard-api-orders-validate-lag-order)
 	LocationId   uint64 `json:"locationId"`
@@ -93,69 +94,6 @@ type OrderPort struct {
 	Virtual      bool   `json:"virtual"` // TODO: False for port, true for MCR1.0 (https://dev.megaport.com/#standard-api-orders-validate-port-order)
 }
 
-type OrderPortConfig struct {
+type portOrderConfig struct {
 	McrASN uint64 `json:"mcrAsn,omitempty"`
-}
-
-type OrderVxc struct {
-	ProductUid     string                   `json:"productUid"`
-	AssociatedVxcs []OrderVxcAssociatedVxcs `json:"associatedVxcs"`
-}
-
-type OrderVxcAssociatedVxcs struct {
-	ProductName string       `json:"productName"`
-	RateLimit   uint64       `json:"rateLimit"`
-	AEnd        *OrderVxcEnd `json:"aEnd,omitempty"`
-	BEnd        *OrderVxcEnd `json:"bEnd"`
-}
-
-type OrderVxcEnd struct {
-	ProductUid string `json:"productUid"`
-	VLan       uint64 `json:"vlan,omitempty"`
-}
-
-func (c *Client) PostPortOrder(o OrderPort) (string, error) {
-	p, err := json.Marshal([]OrderPort{o})
-	if err != nil {
-		return "", err
-	}
-	b := bytes.NewBuffer(p)
-	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/v2/networkdesign/buy", c.BaseURL), b)
-	if err != nil {
-		return "", err
-	}
-	d := []map[string]interface{}{}
-	if err := c.do(req, &d); err != nil {
-		return "", err
-	}
-	return d[0]["technicalServiceUid"].(string), nil
-}
-
-func (c *Client) PostVxcOrder(o []OrderVxc) error {
-	if o == nil {
-		o = []OrderVxc{
-			OrderVxc{
-				ProductUid: "94ec5655-5bef-4734-b172-97f3aed05382",
-				AssociatedVxcs: []OrderVxcAssociatedVxcs{
-					OrderVxcAssociatedVxcs{
-						ProductName: "bar",
-						RateLimit:   100,
-						BEnd: &OrderVxcEnd{
-							ProductUid: "f2c5b25b-e202-4708-9c25-1130c94689b3",
-							VLan:       99,
-						},
-					},
-				},
-			},
-		}
-	}
-	s, err := json.MarshalIndent(o, "", "  ")
-	fmt.Printf("%+v\n", err)
-	fmt.Printf("%s\n", s)
-	b := bytes.NewBuffer(s)
-	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/v2/networkdesign/buy", c.BaseURL), b)
-	if err != nil {
-		return nil
-	}
-	return c.do(req, nil)
 }
