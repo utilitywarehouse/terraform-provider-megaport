77de8b8d2dd3ef492b819ad590e1f124647363d3
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-10-10 15:46:16 +0100

    Reuse bits of private_vxc in the port resource
    Since it's a "container" for vxc connections, the schema is equivalent
    and it's better to maintain the login in one place.
    
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- megaport/resource_megaport_port.go
+++ megaport/resource_megaport_port.go
@@ -43,12 +43,11 @@ func resourceMegaportPort() *schema.Resource {
 				Optional: true,
 			},
 			"associated_vxcs": {
-				Type:       schema.TypeSet,
-				Optional:   true,
-				Computed:   true,
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem:       vxcResource,
-				Set:        schema.HashResource(vxcResource),
+				Type:     schema.TypeSet,
+				Optional: true,
+				Computed: true,
+				Elem:     resourceMegaportPrivateVxc(),
+				Set:      schema.HashResource(resourceMegaportPrivateVxc()),
 			},
 			"marketplace_visibility": {
 				Type:     schema.TypeString,
@@ -70,70 +69,6 @@ func resourceMegaportPort() *schema.Resource {
 	}
 }
 
-var (
-	// TODO: should these be functions?
-	vxcResource = &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"rate_limit": {
-				Type:     schema.TypeInt,
-				Required: true,
-			},
-			"a_end": {
-				Type:       schema.TypeSet,
-				Optional:   true,
-				Computed:   true,
-				MaxItems:   1,
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem:       vxcAEndResource,
-			},
-			"b_end": {
-				Type:       schema.TypeSet,
-				Optional:   true,
-				Computed:   true,
-				MaxItems:   1,
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem:       vxcBEndResource,
-			},
-			"invoice_reference": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-		},
-	}
-
-	vxcAEndResource = &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"product_uid": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"vlan": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			// TODO: untag?
-			// TODO: product_uid might be needed for independant?
-		},
-	}
-
-	vxcBEndResource = &schema.Resource{
-		Schema: map[string]*schema.Schema{
-			"product_uid": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"vlan": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-		},
-	}
-)
-
 func resourceMegaportPortRead(d *schema.ResourceData, m interface{}) error {
 	cfg := m.(*Config)
 	p, err := cfg.Client.Port.Get(d.Id())
@@ -151,16 +86,11 @@ func resourceMegaportPortRead(d *schema.ResourceData, m interface{}) error {
 		vxcs[i] = map[string]interface{}{
 			"name":       v.ProductName,
 			"rate_limit": int(v.RateLimit),
-			"a_end": schema.NewSet(schema.HashResource(vxcAEndResource), []interface{}{map[string]interface{}{
-				"vlan": int(v.AEnd.Vlan),
-			}}),
-			"b_end": schema.NewSet(schema.HashResource(vxcBEndResource), []interface{}{map[string]interface{}{
-				"product_uid": v.BEnd.ProductUid,
-				"vlan":        int(v.BEnd.Vlan),
-			}}),
+			"a_end":      flattenVxcEnd(v.AEnd),
+			"b_end":      flattenVxcEnd(v.BEnd),
 		}
 	}
-	d.Set("associated_vxcs", schema.NewSet(schema.HashResource(vxcResource), vxcs))
+	d.Set("associated_vxcs", schema.NewSet(schema.HashResource(resourceMegaportPrivateVxc()), vxcs))
 	d.Set("marketplace_visibility", p.MarketplaceVisibility)
 	//d.Set("invoice_reference", p.) // TODO: is this even exported?
 	return nil
