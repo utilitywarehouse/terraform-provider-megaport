fce6735a1da1820f8f331eb987a56e6ea90838b3
Dimitrios Karagiannis <dhkarag@gmail.com>
2019-11-04 16:56:39 +0000

    api: pointer conversion functions should return nil for nil input
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- megaport/api/helpers.go
+++ megaport/api/helpers.go
@@ -1,21 +1,33 @@
 package api
 
 func String(v interface{}) *string {
+	if v == nil {
+		return nil
+	}
 	r := v.(string)
 	return &r
 }
 
 func Uint64(v interface{}) *uint64 {
+	if v == nil {
+		return nil
+	}
 	r := v.(uint64)
 	return &r
 }
 
 func Uint64FromInt(v interface{}) *uint64 {
+	if v == nil {
+		return nil
+	}
 	r := uint64(v.(int))
 	return &r
 }
 
 func Bool(v interface{}) *bool {
+	if v == nil {
+		return nil
+	}
 	r := v.(bool)
 	return &r
 }
