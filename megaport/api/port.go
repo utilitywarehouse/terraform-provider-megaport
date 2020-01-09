2169c079901c7efc9ee8b487cde9543d3d0881f7
Dimitrios Karagiannis <dhkarag@gmail.com>
2020-01-09 17:05:13 +0000

    megaport_port: add marketplace_visibility attribute
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- megaport/api/port.go
+++ megaport/api/port.go
@@ -18,16 +18,17 @@ const (
 // mcr2: virtual = false, type = MCR2
 
 type portCreatePayload struct {
-	CreateDate   *uint64 `json:"createDate,omitempty"`   // TODO: need to fill in? :o
-	LagPortCount *uint64 `json:"lagPortCount,omitempty"` // TODO: Required: the number of ports in this LAG order (https://dev.megaport.com/#standard-api-orders-validate-lag-order)
-	LocationId   *uint64 `json:"locationId"`
-	LocationUid  *string `json:"locationUid,omitempty"` // TODO: null in example, is it a string? https://dev.megaport.com/#standard-api-orders-validate-port-order
-	Market       *string `json:"market,omitempty"`      // TODO: what is this ???
-	PortSpeed    *uint64 `json:"portSpeed"`             // TODO: validate 1000, 10000, 100000
-	ProductName  *string `json:"productName"`
-	ProductType  *string `json:"productType"` // TODO: "MEGAPORT"?
-	Term         *uint64 `json:"term"`
-	Virtual      *bool   `json:"virtual"` // TODO: False for port, true for MCR1.0 (https://dev.megaport.com/#standard-api-orders-validate-port-order)
+	CreateDate            *uint64 `json:"createDate,omitempty"`   // TODO: need to fill in? :o
+	LagPortCount          *uint64 `json:"lagPortCount,omitempty"` // TODO: Required: the number of ports in this LAG order (https://dev.megaport.com/#standard-api-orders-validate-lag-order)
+	LocationId            *uint64 `json:"locationId"`
+	LocationUid           *string `json:"locationUid,omitempty"` // TODO: null in example, is it a string? https://dev.megaport.com/#standard-api-orders-validate-port-order
+	Market                *string `json:"market,omitempty"`      // TODO: what is this ???
+	PortSpeed             *uint64 `json:"portSpeed"`             // TODO: validate 1000, 10000, 100000
+	ProductName           *string `json:"productName"`
+	ProductType           *string `json:"productType"` // TODO: "MEGAPORT"?
+	Term                  *uint64 `json:"term"`
+	Virtual               *bool   `json:"virtual"` // TODO: False for port, true for MCR1.0 (https://dev.megaport.com/#standard-api-orders-validate-port-order)
+	MarketplaceVisibility *bool   `json:"marketplaceVisibility,omitempty"`
 }
 
 type portUpdatePayload struct {
@@ -38,10 +39,11 @@ type portUpdatePayload struct {
 }
 
 type PortCreateInput struct {
-	LocationId *uint64
-	Name       *string
-	Speed      *uint64
-	Term       *uint64
+	LocationId            *uint64
+	MarketplaceVisibility *bool
+	Name                  *string
+	Speed                 *uint64
+	Term                  *uint64
 }
 
 func (v *PortCreateInput) productType() string {
@@ -50,12 +52,13 @@ func (v *PortCreateInput) productType() string {
 
 func (v *PortCreateInput) toPayload() ([]byte, error) {
 	payload := []*portCreatePayload{{
-		LocationId:  v.LocationId,
-		PortSpeed:   v.Speed,
-		ProductName: v.Name,
-		ProductType: String(ProductTypePort), // TODO
-		Term:        v.Term,
-		Virtual:     Bool(false), // TODO
+		LocationId:            v.LocationId,
+		PortSpeed:             v.Speed,
+		ProductName:           v.Name,
+		ProductType:           String(ProductTypePort), // TODO
+		Term:                  v.Term,
+		Virtual:               Bool(false), // TODO
+		MarketplaceVisibility: v.MarketplaceVisibility,
 	}}
 	return json.Marshal(payload)
 }
