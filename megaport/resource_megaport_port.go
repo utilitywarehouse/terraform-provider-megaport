c5176e8f355d6d2a65d5cca6c3df87259aa11052
Dimitrios Karagiannis <dhkarag@gmail.com>
2020-02-03 13:04:48 +0000

    Add missing imports
    Signed-off-by: Dimitrios Karagiannis <dhkarag@gmail.com>

--- megaport/resource_megaport_port.go
+++ megaport/resource_megaport_port.go
@@ -2,7 +2,9 @@ package megaport
 
 import (
 	"log"
+	"time"
 
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/utilitywarehouse/terraform-provider-megaport/megaport/api"
